2067ddb9d62a9dbea6339ea2f976ed4e26d70385
nilsantana <nilsantana@yahoo.com.br>
2018-10-17 20:01:47 -0300

    correção variáveis locais

--- youtube.sh
+++ youtube.sh
@@ -13,29 +13,37 @@
 #inscritos sed -n '/subscriber-count/{p;q;}' nome5.html | sed 's/.*subscriber-count//g' | sed 's/<[^>]*>//g;s/.*>//g'
 
 function youtube(){
-	
-	local _video=$(mktemp)
-	local _channel=$(mktemp)
-	local _url="https://youtube.com/channel"
-	wget "$1" -O "$_video" 2>/dev/null
-	
-	local _title=$(grep '<title>' "$_video" | sed 's/<[^>]*>//g' | sed 's/ - You.*//g')
-	local _views=$(grep 'watch-view-count' "$_video" | sed 's/<[^>]*>//g')
-	local _likes=$(grep 'like-button-renderer-like-button' "$_video"  |sed -n 1p | sed 's/<[^>]*>//g;s/ //g')
-	local _dislikes=$(grep 'like-button-renderer-dislike-button'  "$_video" |sed -n 1p | sed 's/<[^>]*>//g;s/ //g')
-	local _id=$(sed 's/channel/\n&/g' "$_video" | grep '^channel'| sed -n 1p | sed 's/isCrawlable.*//g;s/..,.*//g;s/.*"//g')
-	wget "$_url/$_id" -O "$_channel" 2>/dev/null
-	
-	local _tchannel=$(sed -n '/title/{p;q;}' "$_channel" | sed 's/<title> //g')
-	local _subscribers=$(sed -n '/subscriber-count/{p;q;}' "$_channel" | sed 's/.*subscriber-count//g' | sed 's/<[^>]*>//g;s/.*>//g')
-	
-	echo "Título do canal $_tchannel"
-	echo "Inscritos: $_subscribers"
-	echo "Titulo do video:  $_title"
-	echo "Visualizações: $_views"
-	echo "Gostei: $_likes"
-	echo "Dislikes: $_dislikes"
 
+        local _video
+         _video=$(mktemp)
+        local _channel
+        _channel=$(mktemp)
+        local _url
+        _url="https://youtube.com/channel"
+        wget "$1" -O "$_video" 2>/dev/null
+
+        local _title
+        _title=$(grep '<title>' "$_video" | sed 's/<[^>]*>//g' | sed 's/ - You.*//g')
+        local _views
+        _views=$(grep 'watch-view-count' "$_video" | sed 's/<[^>]*>//g')
+        local _likes
+        _likes=$(grep 'like-button-renderer-like-button' "$_video"  |sed -n 1p | sed 's/<[^>]*>//g;s/ //g')
+        local _dislikes
+        _dislikes=$(grep 'like-button-renderer-dislike-button'  "$_video" |sed -n 1p | sed 's/<[^>]*>//g;s/ //g')
+        local _id
+        _id=$(sed 's/channel/\n&/g' "$_video" | grep '^channel'| sed -n 1p | sed 's/isCrawlable.*//g;s/..,.*//g;s/.*"//g')
+        wget "$_url/$_id" -O "$_channel" 2>/dev/null
+
+        local _tchannel
+        _tchannel=$(sed -n '/title/{p;q;}' "$_channel" | sed 's/<title> //g')
+        local _subscribers
+        _subscribers=$(sed -n '/subscriber-count/{p;q;}' "$_channel" | sed 's/.*subscriber-count//g' | sed 's/<[^>]*>//g;s/.*>//g')
+        echo "Título do canal $_tchannel"
+        echo "Inscritos: $_subscribers"
+        echo "Titulo do video:  $_title"
+        echo "Visualizações: $_views"
+        echo "Gostei: $_likes"
+        echo "Dislikes: $_dislikes"
 }
 
 
